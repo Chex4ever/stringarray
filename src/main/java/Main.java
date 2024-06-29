e44859fceb1eafaa8a1bc68d8fb2e97b48d776f6
Exever <4ex4ever4ex@gmail.com>
2024-06-29 15:24:18 +0500

    Homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
-        List<String> example = new ArrayList<>();
-        example.equals(example);
     }
 
 }
